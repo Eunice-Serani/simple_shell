bc07d971328ddd90d27e42b8d26a6f852e6a5b30
Eunice-Serani <eaamable91@gmail.com>
2023-10-14 12:44:12 -0700

    My shell project

--- _builtin_elements.c
+++ _builtin_elements.c
@@ -1,4 +1,5 @@
 #include  "myshell.h"
+
 /**
  * exit_shell - Terminate the shell with an optional exit status.
  * @shell_info: A structure holding potential arguments, maintaining
@@ -9,24 +10,24 @@
  */
 int exit_shell(shell_info_t *shell_info)
 {
-    int exit_status;
+	int exit_status;
 
-    if (shell_info->arguments[1])  /* Check for an exit argument */
-    {
-        exit_status = _convert_to_int(shell_info->arguments[1]);
-        if (exit_status == -1)
-        {
-            shell_info->status = 2;
-            display_error(shell_info, "Invalid exit status: ");
-            print_string(shell_info->arguments[1]);
-            print_character('\n');
-            return (1);
-        }
-        shell_info->error_number = _convert_to_int(shell_info->arguments[1]);
-        return (-2);
-    }
-    shell_info->error_number = -1;
-    return (-2);
+	if (shell_info->arguments[1])  /* Check for an exit argument */
+	{
+		exit_status = _convert_to_int(shell_info->arguments[1]);
+		if (exit_status == -1)
+		{
+			shell_info->status = 2;
+			display_error(shell_info, "Invalid exit status: ");
+			print_string(shell_info->arguments[1]);
+			print_character('\n');
+			return (1);
+		}
+		shell_info->error_number = _convert_to_int(shell_info->arguments[1]);
+		return (-2);
+	}
+	shell_info->error_number = -1;
+	return (-2);
 }
 /**
  * change_current_directory - Modify the current working directory of the process.
@@ -86,13 +87,13 @@ int change_current_directory(info_t *info)
  */
 int display_help(info_t *info)
 {
-    char **argument_array;
+	char **argument_array;
 
-    argument_array = info->argv;
-    _puts("The 'help' function call works, but the function is not yet implemented.\n");
-    if (0)
-        _puts(*argument_array); /* Temporary workaround to suppress unused variable warning */
-    return (0);
+	argument_array = info->argv;
+	_puts("The 'help' function call works, but the function is not yet implemented.\n");
+	if (0)
+		_puts(*argument_array); /* Temporary workaround to suppress unused variable warning */
+	return (0);
 }
 /**
  * show_history - Display the command history with line numbers.
@@ -102,17 +103,16 @@ int display_help(info_t *info)
  */
 int show_history(info_t *info)
 {
-    print_command_history(info->history);
-    return 0;
+	print_command_history(info->history);
+	return 0;
 }
-
 /**
  * Unset an alias by making it empty.
  *
  * @param info A data structure containing relevant information.
  * @param alias The alias to unset.
  *
- * @return 0 on success, 1 on failure.
+ * Return: 0 on success, 1 on failure.
  */
 int unset_alias(info_t *info, char *alias)
 /**
@@ -124,11 +124,11 @@ int unset_alias(info_t *info, char *alias)
  */
 int unset_alias(info_t *info, char *alias)
 {
-    char *p = _strchr(alias, '=');
-    if (!p) return 1;
-    char c = *p;
-    *p = 0;
-    int r = delete_node_at_index(&(info->alias), get_node_index(info->alias, node_starts_with(info->alias, alias, -1)));
-    *p = c;
-    return r;
+	char *p = _strchr(alias, '=');
+	if (!p) return 1;
+	char c = *p;
+	*p = 0;
+	int r = delete_node_at_index(&(info->alias), get_node_index(info->alias, node_starts_with(info->alias, alias, -1)));
+	*p = c;
+	return r;
 }
